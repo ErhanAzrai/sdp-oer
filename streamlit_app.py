e1c86cbe8531275e76001c87533c51da7f36f1b1
Erhan Azrai <erhan.azrai@gmail.com>
2023-09-21 11:03:27 +0800

    changes to sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
 import plotly_express as px
+import plotly.graph_objects as go
 
-def stats(dataframe):
-    st.header("Data Statistics")
-    st.write(dataframe.describe())
+def plotOER(dataframe):
+    st.header("Oil Extraction Rate Performance")
+
+    fig = px.line(df, x="date", y="%_OER", color="mill_code")
+    #fig.update_traces(textposition="bottom right")
+    st.plotly_chart(fig)
 
 def data_header(dataframe):
     st.header("Data Header")
@@ -18,11 +22,14 @@ def data_plot(dataframe):
 
 
     #col = st.color_picker('Select a plot color')
-    mill_042 = dataframe.loc[dataframe['mill_code']==millcode]
+    mill_code = dataframe.loc[dataframe['mill_code']==millcode]
 
-    plot = px.scatter(mill_042, x=x_axis_val, y = y_axis_val)
+    plot = px.scatter(mill_code, x=x_axis_val, y = y_axis_val)
     #plot.update_traces(marker=(dict(color=col)))
     st.plotly_chart(plot)
+
+def home_page():
+    st.write('OER = (Quality of Crop) + (Quality of Ripeness) + (Efficiency of Mill)')
 
 st.title("Oil Extraction Rate Data Explorer")
 st.text("This is a POC for OER by SDPKS")
@@ -31,7 +38,7 @@ st.text("This is a POC for OER by SDPKS")
 st.sidebar.title('Navigation')
 uploaded_file = st.sidebar.file_uploader("Upload your file here")
 
-options = st.sidebar.radio('Operations', options=['Home','Data Statistics', 'Data Header', 'Plot'])
+options = st.sidebar.radio('Operations', options=['Home','OER Performance', 'Data Header', 'Exploratory Plot'])
 
 
 if uploaded_file:
@@ -44,9 +51,11 @@ if uploaded_file:
 
 
 
-if options == "Data Statistics":
-    stats(df)
+if options == "OER Performance":
+    plotOER(df)
 elif options =='Data Header':
     data_header(df)
-elif options == 'Plot':
+elif options == 'Exploratory Plot':
     data_plot(df)
+elif options =='Home':
+    home_page()
